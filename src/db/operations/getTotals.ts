10cdd6e6993dd5ff4419676f2117502490ad49fe
Gabriel De Almeida <52146328+gabedealmeida@users.noreply.github.com>
2023-05-30 11:29:33 -0400

    Changed the getTotals function documentation

--- src/db/operations/getTotals.ts
+++ src/db/operations/getTotals.ts
@@ -1,22 +1,17 @@
-import { QueryTypes, ITotal } from '../../@types'
+import { QueryTypes, ITotal, ITotalsRequest } from '../../@types'
 import { sequelize } from '../../models'
 import { getTotalsForContracts } from '../../models/queries'
 
 /**
- * Get the token balances for the passed in wallet adresses, or the token balances under the passed in contract addresses, or the token balances for the passed in wallet addresses that are also part of the passed in contract addresses.
+ * Get the number of token holders for the passed in contract addresses.
  *
- * @remarks
- * This function handles a total of three querying scenarios based around which arguments it's called with.
+ * @param {ITotalsRequest} IGetQuery - The contracts arrays to get the holders for.
  *
- * @param {IGetQuery} IGetQuery - The wallets and/or contracts arrays to get the balances for.
- *
- * @returns {Promise<IBalance[]>} Promise<IBalance[]> A promise that resolves to an array of balances.
+ * @returns {Promise<ITotal[]>} Promise<ITotal[]> A promise that resolves to an array of holders.
  */
 export default async function getTotals({
   contracts
-}: {
-  contracts: string[]
-}): Promise<ITotal[]> {
+}: ITotalsRequest): Promise<ITotal[]> {
   const query = getTotalsForContracts
 
   return sequelize.query<ITotal>(query, {
